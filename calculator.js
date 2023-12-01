14fccd2983cc3c6bba2ef8bd2179a993fe30e801
Shahzad-Minhaj <sminhaj.suit@gmail.com>
2023-12-01 11:59:12 +0500

    second commit

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
+/** Assignment # 2
+Waseem and Ali are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+and height in meter).
+
+Your tasks:
+1. Store Ali's and Waseem's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both 
+versions)
+3. Create a Boolean variable 'WaseemHigherBMI' containing information about 
+whether Waseem has a higher BMI than Ali.
+Test data:
+* Data 1: Waseem weights 78 kg and is 1.69 m tall. Ali weights 92 kg and is 1.95 
+m tall.
+*Data 2: Waseem  weights 95 kg and is 1.88 m tall. Ali weights 85 kg and is 1.76 
+m tall.
+ */
+
 // // Data 1
 var waseemWeight1 = 78; // in kg
 var waseemHeight1 = 1.69; // in meters
